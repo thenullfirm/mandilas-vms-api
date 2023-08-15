05789908ae3a458d924f7928b727cb8bbad61fec
nullthefirst <tey@nullnow.com>
2023-08-15 12:19:39 +0100

    Updated visitor model

--- src/models/Visitor.js
+++ src/models/Visitor.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const Employee = require('./Employee.js');
+const Employee = require('./Employee');
 
 const visitorSchema = new mongoose.Schema({
   visitorName: {
